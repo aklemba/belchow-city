645ce57014dace8765c8ae6f8e37b2ef91154476
Adam Klemba <adam.a.klemba@gmail.com>
2022-04-03 15:37:28 +0200

    Add initial project

---
+++ app/src/main/java/com/klemba/belchowcity/MeetingActivity.kt
@@ -0,0 +1,12 @@
+package com.klemba.belchowcity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MeetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.meeting_activity)
+    }
+}
